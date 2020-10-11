2d205bc0fd5ac1bee740712c3466eb30253873c7
Nick Guimarais <Nick.guimarais@gmail.com>
2020-10-11 16:07:59 -0700

    updating routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     return (
         <Router>
         <Navbar />
-            <Route exact path="/" component={About} />
+            <Route exact path="/React-Portfolio" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={PortfolioPage} />
             <Route exact path="/contact" component={Contact} />
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,16 +10,16 @@ function Navbar() {
     return (
      
       <nav className="navbar navbar-expand-lg navbar-light text-left" id="nav-color">
-      <Link className="navbar-brand" to="/" id="navbar-text">
+      <Link className="navbar-brand" to="/React-Portfolio" id="navbar-text">
         Navbar
       </Link>
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link id="about-text"
-              to="/"
+              to="/React-Portfolio"
               className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
+                window.location.pathname === "/React-Portfolio" || window.location.pathname === "/about"
                   ? "nav-link active"
                   : "nav-link"
               }
@@ -48,27 +48,6 @@ function Navbar() {
     </nav>
 
    
-
-
-      //   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      //   <a className="navbar-brand" href="#">Navbar</a>
-      //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      //     <span className="navbar-toggler-icon"></span>
-      //   </button>
-      //   <div className="collapse navbar-collapse" id="navbarNav">
-      //     <ul className="navbar-nav">
-      //       <li className="nav-item active">
-      //         <a className="nav-link" href="index.html">About <span className="sr-only">(current)</span></a>
-      //       </li>
-      //       <li className="nav-item">
-      //         <a className="nav-link" href="portfolio.html">Portfolio</a>
-      //       </li>
-      //       <li className="nav-item">
-      //         <a className="nav-link" href="contact.html">Contact</a>
-      //       </li>
-      //     </ul>
-      //   </div>
-      // </nav>
 
     )
 }
