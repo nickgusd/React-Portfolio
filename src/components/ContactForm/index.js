4340f5e726a8c27192ec2baa88126991a73c93d4
Nick Guimarais <Nick.guimarais@gmail.com>
2020-10-12 14:54:29 -0700

    updating contact page

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,23 +1,51 @@
 import React from "react";
 import {Form, Button} from 'react-bootstrap';
+import {InlineWidget} from "react-calendly";
+import emailjs from 'emailjs-com';
+import{ init } from 'emailjs-com';
 import "./style.css";
 
+
+// init("user_wfPJJXSH9xWczqIkxR4iP");
+
 function ContactForm() {
+
+  init("user_wfPJJXSH9xWczqIkxR4iP");
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_py7vh7h', 'template_hjf2hmj', e.target, 'user_wfPJJXSH9xWczqIkxR4iP')
+      .then((result) => {
+          console.log(result.text);
+          window.location.reload();
+
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+  
+
+
     return (
         
-<Form>
+
+
+//form contains widget for Calendly
+
+<Form onSubmit={sendEmail}>
     <header>
       Contact
     </header>
     <hr/>
     <Form.Group controlId="formBasicName">
     <Form.Label>Name</Form.Label>
-    <Form.Control type="name" placeholder="Enter name" />
+    <Form.Control type="name" placeholder="Enter name" name="from_name" id="from_name" />
   </Form.Group>
 
     <Form.Group controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control  placeholder="Enter email"  type="text" name="from_email" id="from_email"/>
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -25,48 +53,17 @@ function ContactForm() {
 
   <Form.Group controlId="exampleForm.ControlTextarea1">
     <Form.Label>Message</Form.Label>
-    <Form.Control as="textarea" rows="3" placeholder="Please leave a message!"/>
+    <Form.Control as="textarea" rows="3" placeholder="Please leave a message!" type="email" name="message" id="message"/>
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button variant="primary" type="submit" >
     Submit
   </Button>
+  <InlineWidget  
+    color="#00a2ff"
+    text="Request a Meeting Time!"
+    textColor="#ffffff"url="https://calendly.com/nick-guimarais"/>
 </Form>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <form>
-    //     <header>
-    //         <h1 className = "contact-header">Contact</h1>
-    //     </header>
-    //     <hr/>
-    //     <div className="form-group">
-    //         <label htmlFor="exampleInputName1">Name</label>
-    //         <input type="name" className="form-control" id="exampleInputName1" placeholder="Nick Guimarais"/>
-    //       </div>
-
-    //     <div className="form-group">
-    //       <label htmlFor="exampleInputEmail1">Email address</label>
-    //       <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@gmail.com"/>
-    //       <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-    //     </div>
-    //     <div className="form-group form-message">
-    //         <header className="message">Message</header>
-    //         <textarea name="message" rows="5" cols="30" id="exampleMessage1">Please leave a message!</textarea>
-    //     </div>
-    //     <button type="submit" className="btn btn-primary">Submit</button>
-    //   </form>
 
 
     )
