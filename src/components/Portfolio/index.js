7180f0da35b446b4e155bbc082bb6dad06a92e13
Nick Guimarais <Nick.guimarais@gmail.com>
2021-10-28 16:13:30 -0700

    refactored portfolio grid

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,16 +1,26 @@
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import Bubble from "../../assets/img/rsz_analysis-blackboard-board-bubble-355952.JPG"
-import Apple from "../../assets/img/rsz_apple-applications-apps-cell-phone-607812.jpg"
-import Quiz from "../../assets/img/rsz_black-flat-screen-computer-monitor-1714208.jpg"
-import Tinder from "../../assets/img/rsz_business-code-coding-computer-270360.jpg"
-import Laptop from "../../assets/img/rsz_computer-desk-hand-laptop-374631.jpg"
-import Ipad from "../../assets/img/rsz_space-grey-ipad-air-with-graph-on-brown-wooden-table-187041.jpg"
-import Projects from "./projects";
-import "./style.css";
+
 import Card from "../Card/index"
 
+import "./style.css";
+
+import { content } from "./projects";
+
+const projectInfo = content.map((item) => {
+  const { projects, deployed, description, github, image } = item;
+  return (
+    <Row className="first-row">
+    {projects.map((project, idx) => {
+      return (
+      <Col xs={12} md={6} sm={12} className="grid-col">
+        <Card description={description[idx]} projects={projects[idx]} github={github[idx]} deployed={deployed[idx]} image={image[idx]}></Card>
+      </Col>
+      )})}
+      </Row>
+  )
+})
 
 function PortfolioGrid() {
   return (
@@ -20,33 +30,7 @@ function PortfolioGrid() {
           <h1 id="portfolio">Nick's Portfolio</h1>
         </Col>
       </Row>
-
-      <Row className="first-row">
-        <Col xs={12} md={6} sm={12} className="grid-col">
-          <Card description={Projects[3].description7} projects={Projects[0].project7} github={Projects[1].github7} deployed={Projects[2].deployed7} image={Bubble}></Card>
-        </Col>
-        <Col xs={12} md={6} sm={12} className="grid-col">
-          <Card description={Projects[3].description5} projects={Projects[0].project5} github={Projects[1].github5} deployed={Projects[2].deployed5} image={Apple}></Card>
-        </Col>
-      </Row>
-
-      <Row className="second-row">
-        <Col xs={12} md={6} sm={12} className="grid-col" >
-          <Card description={Projects[3].description3} projects={Projects[0].project3} github={Projects[1].github3} deployed={Projects[2].deployed3} image={Quiz}></Card>
-        </Col>
-        <Col xs={12} md={6} sm={12} className="grid-col" >
-          <Card description={Projects[3].description4} projects={Projects[0].project4} github={Projects[1].github4} deployed={Projects[2].deployed4} image={Tinder}></Card>
-        </Col>
-      </Row>
-
-      <Row className="third-row">
-        <Col xs={12} md={6} sm={12} className="grid-col">
-          <Card description={Projects[3].description2} projects={Projects[0].project2} github={Projects[1].github2} deployed={Projects[2].deployed2} image={Laptop}></Card>
-        </Col>
-        <Col xs={12} md={6} sm={12} className="grid-col">
-          <Card description={Projects[3].description1} projects={Projects[0].project1} github={Projects[1].github1} deployed={Projects[2].deployed1} image={Ipad}></Card>
-        </Col>
-      </Row>
+      {projectInfo}
     </Container>
   )
 }
--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -1,45 +1,39 @@
+import Bubble from "../../assets/img/rsz_analysis-blackboard-board-bubble-355952.JPG"
+import Apple from "../../assets/img/rsz_apple-applications-apps-cell-phone-607812.jpg"
+import Quiz from "../../assets/img/rsz_black-flat-screen-computer-monitor-1714208.jpg"
+import Tinder from "../../assets/img/rsz_business-code-coding-computer-270360.jpg"
+import Laptop from "../../assets/img/rsz_computer-desk-hand-laptop-374631.jpg"
+import Ipad from "../../assets/img/rsz_space-grey-ipad-air-with-graph-on-brown-wooden-table-187041.jpg"
 
-
-const projects = [
-    {
-        project1: "Secure Travel App",
-        project2: "Note-Taker",
-        project3: "Music eXchange",
-        project4: "Tinder for Restaurants",
-        project5: "Ernie Ball Landing Page",
-        project6: "Google Book Search",
-        project7: "VaxDash.us"
-
+export const content = [
+   {
+        projects: ["VaxDash.us", "Ernie Ball Landing Page"],
+        github: ["https://github.com/Lifeshack/vax-dash","https://github.com/nickgusd/Landing-Page" ],
+        deployed: ["https://vaxdash.us/", "https://fathomless-basin-76735.herokuapp.com/"],
+        description: [
+            "This platform allows users to make an informed decision about whether or not to get the COVID-19 vaccine, view the vaccination progress, determine vaccine eligibility in their state, and locate vaccine clinics.",
+            "This website is a product landing page inspired by my favorite guitar series, the Ernie Ball Music Man!"
+        ],
+        image: [Bubble, Apple]
+    },
+{
+        projects: ["Music eXchange", "Tinder for Restaurants"],
+        github: ["https://github.com/nickgusd/Muse-eXchange","https://github.com/tpacba/tinderforrestaurants" ],
+        deployed: ["https://musicexchangev2.herokuapp.com/", "https://vast-lowlands-43566.herokuapp.com/"],
+        description: [
+            "An application that allows musicians and artists to their sell music and tutorials directly to their fans.",
+            "An application that helps a group of people choose a restaurant through rounds of swiping, similar to Tinder UI. When the group narrows down the restaurants that they like to 1 restaurant, the group is presented with a match."
+        ],
+        image: [Quiz, Tinder]
     },
     {
-        github1: "https://github.com/edwardreyes29/project-1",
-        github2: "https://github.com/nickgusd/Note-Taker",
-        github3: "https://github.com/nickgusd/Muse-eXchange",
-        github4: "https://github.com/tpacba/tinderforrestaurants",
-         github5: "https://github.com/nickgusd/Landing-Page",
-        github6: "https://github.com/nickgusd/Google-Book-Search",
-        github7: "https://github.com/Lifeshack/vax-dash"
-
+        projects: ["Note-Taker", "Secure Travel App"],
+        github: ["https://github.com/nickgusd/Note-Taker","https://github.com/edwardreyes29/project-1" ],
+        deployed: ["https://protected-shore-03622.herokuapp.com/", "https://edwardreyes29.github.io/project-1/"],
+        description: [
+            "An application that has the ability to save, delete, and view old notes.",
+            "A travel app that advises the user where they should travel based on Coronavirus risk recommendations, and provides the user with travel accommodation options."
+        ],
+        image: [Laptop, Ipad]
     },
-    {
-        deployed1: "https://edwardreyes29.github.io/project-1/",
-        deployed2: "https://protected-shore-03622.herokuapp.com/",
-        deployed3: "https://musicexchangev2.herokuapp.com/",
-        deployed4: "https://vast-lowlands-43566.herokuapp.com/",
-        deployed5: "https://fathomless-basin-76735.herokuapp.com/",
-        deployed6: "https://damp-waters-57453.herokuapp.com/",
-        deployed7: "https://vaxdash.us/"
-    },
-    {
-        description1: "A travel app that advises the user where they should travel based on Coronavirus risk recommendations, and provides the user with travel accommodation options.",
-        description2: "An application that has the ability to save, delete, and view old notes.",
-        description3: "An application that allows musicians and artists to their sell music and tutorials directly to their fans.",
-        description4: "An application that helps a group of people choose a restaurant through rounds of swiping, similar to Tinder UI. When the group narrows down the restaurants that they like to 1 restaurant, the group is presented with a match.",
-        description5: "This website is a product landing page inspired by my favorite guitar series, the Ernie Ball Music Man!",
-        description6: "This application allows the user to search for a book with the Google Search API. The user can also view the book they searched for on Google Books, and the user can even save their favorite books to be stored in the database.",
-        description7: "This platform allows users to make an informed decision about whether or not to get the COVID-19 vaccine, view the vaccination progress, determine vaccine eligibility in their state, and locate vaccine clinics."
-    }
 ]
-
-
-export default projects; 
